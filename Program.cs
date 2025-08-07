f946db305426a5c8ff0131080a092a92bf1bf104
wareken <93680044+wareken@users.noreply.github.com>
2025-08-07 11:22:24 -0500

    Initial Commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, C# Academy! Keeping only one message");
+
+void PrintFirstMessage(string message)
+{
+    //Do Something with the message
+}
+
+void PrintSecondMessage(string message)
+{
+    //Do Something with the message
+}
+
+void PrintThirdMessage(string message)
+{
+    //Do Something with the message
+}
