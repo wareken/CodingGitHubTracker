6fa176bc960921ea736c0bcabc1485d51cdeef01
wareken <93680044+wareken@users.noreply.github.com>
2025-08-07 11:48:47 -0500

    Let's keep one print method via GitHub

--- Program.cs
+++ Program.cs
@@ -1,17 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C# Academy! Keeping only one message");
+Console.WriteLine("Hello, C# Academy! Let's keep on print Method.");
 
 void PrintFirstMessage(string message)
-{
-    //Do Something with the message
-}
-
-void PrintSecondMessage(string message)
-{
-    //Do Something with the message
-}
-
-void PrintThirdMessage(string message)
 {
     //Do Something with the message
 }
