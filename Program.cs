9a4791ffb6edc1ec5216b16e5e8d470aa2af9f0b
wareken <93680044+wareken@users.noreply.github.com>
2025-08-07 14:00:41 -0500

    Added Four Method

--- Program.cs
+++ Program.cs
@@ -1,7 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C# Academy! Let's keep on print Method.");
+Console.WriteLine("Hello, C# Academy! Keeping only one message");
 
 void PrintFirstMessage(string message)
+{
+    //Do Something with the message
+}
+
+void PrintSecondMessage(string message)
+{
+    //Do Something with the message
+}
+
+void PrintThirdMessage(string message)
+{
+    //Do Something with the message
+}
+
+void PrintFourthMessage(string message)
 {
     //Do Something with the message
 }
